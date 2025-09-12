ce9c91e435a3ad2361d7f20f17a03e8e43559025
inspection-philosophie-lille <daniel.boulagnon@ac-lille.fr>
2025-09-13 01:12:46 +0200

    Add files via upload

---
+++ fichiers/φ - créer des applications - 01 - classer, apparier par des liens/reperes (long).js
@@ -0,0 +1,137 @@
+const reperes = [
+  {
+    "definition": "mot1/mot2",
+    "parts": [
+      {
+        "name": "mot 1",
+        "definition": "définition 1 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 1.1",
+          "exemple 1.2",
+          "exemple 1.3",
+          "exemple 1.4",
+          "exemple 1.5"
+        ]
+      },
+      {
+        "name": "mot 2",
+        "definition": "définition 2 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 2.1",
+          "exemple 2.2",
+          "exemple 2.3",
+          "exemple 2.4",
+          "exemple 2.5"
+        ]
+      }
+    ]
+  },
+  {
+    "definition": "mot3/mot4",
+    "parts": [
+      {
+        "name": "mot 3",
+        "definition": "définition 3 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 3.1",
+          "exemple 3.2",
+          "exemple 3.3",
+          "exemple 3.4",
+          "exemple 3.5"
+        ]
+      },
+      {
+        "name": "mot 4",
+        "definition": "définition 4 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 4.1",
+          "exemple 4.2",
+          "exemple 4.3",
+          "exemple 4.4",
+          "exemple 4.5"
+        ]
+      }
+    ]
+  },
+  {
+    "definition": "mot5/mot6",
+    "parts": [
+      {
+        "name": "mot 5",
+        "definition": "définition 5 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 5.1",
+          "exemple 5.2",
+          "exemple 5.3",
+          "exemple 5.4",
+          "exemple 5.5"
+        ]
+      },
+      {
+        "name": "mot 6",
+        "definition": "définition 6 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 6.1",
+          "exemple 6.2",
+          "exemple 6.3",
+          "exemple 6.4",
+          "exemple 6.5"
+        ]
+      }
+    ]
+  },
+  {
+    "definition": "mot7/mot8",
+    "parts": [
+      {
+        "name": "mot 7",
+        "definition": "définition 7 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 7.1",
+          "exemple 7.2",
+          "exemple 7.3",
+          "exemple 7.4",
+          "exemple 7.5"
+        ]
+      },
+      {
+        "name": "mot 8",
+        "definition": "définition 8 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 8.1",
+          "exemple 8.2",
+          "exemple 8.3",
+          "exemple 8.4",
+          "exemple 8.5"
+        ]
+      }
+    ]
+  },
+  {
+    "definition": "mot9/mot10",
+    "parts": [
+      {
+        "name": "mot 9",
+        "definition": "définition 9 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 9.1",
+          "exemple 9.2",
+          "exemple 9.3",
+          "exemple 9.4",
+          "exemple 9.5"
+        ]
+      },
+      {
+        "name": "mot 10",
+        "definition": "définition 10 De plus en plus de manuels scolaires proposent des textes ou des documents enrichis, c’est-à-dire associés à un QR Code, que les élèves peuvent flasher avec leur smartphone afin d’accéder à des ressources numériques pertinentes et directement en lien avec ce que le texte ou le document aborde. En suivant les quelques étapes indiquées ci-dessous, il est possible de proposer aux élèves des documents comportant de tels QR Codes.",
+        "examples": [
+          "exemple 10.1",
+          "exemple 10.2",
+          "exemple 10.3",
+          "exemple 10.4",
+          "exemple 10.5"
+        ]
+      }
+    ]
+  }
+]
