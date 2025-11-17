4d9ba18f013e433b16339ad751a574968ac8adf4
inspection-philosophie-lille <daniel.boulagnon@ac-lille.fr>
2025-11-17 19:03:41 +0100

    Add files via upload

---
+++ fichiers/φ - créer des applications - 01 - « apparier »/fichier_ressource.js
@@ -0,0 +1,15 @@
+const title = "application « apparier »";
+const subtitle = "associer un élément à un autre";
+
+const argumentsData = [
+	{ colonne_de_gauche: "mot 1", colonne_de_droite: "mot 1'" }, /* remplacer "mot 1" par le premier couple de mot à relier et "mot 1'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 2", colonne_de_droite: "mot 2'" }, /* remplacer "mot 2" par le premier couple de mot à relier et "mot 2'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 3", colonne_de_droite: "mot 3'" }, /* remplacer "mot 3" par le premier couple de mot à relier et "mot 3'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 4", colonne_de_droite: "mot 4'" }, /* remplacer "mot 4" par le premier couple de mot à relier et "mot 4'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 5", colonne_de_droite: "mot 5'" }, /* remplacer "mot 5" par le premier couple de mot à relier et "mot 5'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 6", colonne_de_droite: "mot 6'" }, /* remplacer "mot 6" par le premier couple de mot à relier et "mot 6'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 7", colonne_de_droite: "mot 7'" }, /* remplacer "mot 7" par le premier couple de mot à relier et "mot 7'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 8", colonne_de_droite: "mot 8'" }, /* remplacer "mot 8" par le premier couple de mot à relier et "mot 8'" par son vis-à-vis en laissant les guillements */
+    { colonne_de_gauche: "mot 9", colonne_de_droite: "mot 9'" },  /* remplacer "mot 9" par le premier couple de mot à relier et "mot 9'" par son vis-à-vis en laissant les guillements */
+	/* on peut ajouter des termes à cette liste, mais il faut terminer en ne mettant surtout pas de virgule après "}" */	
+];
---
+++ fichiers/φ - créer des applications - 04 - « questionner »/quiz-questions.js
@@ -0,0 +1,82 @@
+const questions = [
+    // Question n°1 : 
+    { 
+        question: "question n°1 (à rédiger) :",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 1"
+    },
+
+    // Question n°2 : 
+    { 
+        question: "question n°2 (à rédiger) :",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 2"
+    },
+
+    // Question n°3 : 
+    { 
+        question: "question n°3 (à rédiger) :",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 3"
+    },
+
+    // Question n°4 : 
+    { 
+        question: "question n°4 (à rédiger) :",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 4"
+    },
+
+    // Question n°5 : 
+    { 
+        question: "question n°5 (à rédiger) :",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 5"
+    },
+
+    // Question n°6 : 
+    { 
+        question: "question n°6 (à rédiger) :",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 6"
+    },
+
+    // Question n°7 : 
+    { 
+        question: "question n°7 (à rédiger) :",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 7"
+    },
+
+    // Question n°8 : 
+    { 
+        question: "question n°8 (à rédiger) :",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 8"
+    },
+
+    // Question n°9 : 
+    { 
+        question: "question n°9 (à rédiger) :",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 9"
+    },
+
+    // Question n°10 : 
+    { 
+        question: "question n°10 (à rédiger) :",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "Explication de la bonne réponse (à rédiger) à la question 10"
+    }
+
+];
