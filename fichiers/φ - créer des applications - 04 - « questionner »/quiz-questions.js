88ce3b9985e52b766690d3416ac61460585af34d
inspection-philosophie-lille <daniel.boulagnon@ac-lille.fr>
2025-12-13 23:50:11 +0100

    Add files via upload

---
+++ fichiers/φ - créer des applications - 04 - « questionner »/quiz-questions.js
@@ -0,0 +1,163 @@
+const questions = [
+
+    // Question n°1  : 
+    { 
+        question: "question n°1 (à rédiger)",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 1"
+    },
+
+    // Question n°2  : 
+    { 
+        question: "question n°2 (à rédiger)",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 2"
+    },
+
+    // Question n°3  : 
+    { 
+        question: "question n°3 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 3"
+    },
+
+    // Question n°4  : 
+    { 
+        question: "question n°4 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 4"
+    },
+
+    // Question n°5  : 
+    { 
+        question: "question n°5 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 5"
+    },
+
+    // Question n°6  : 
+    { 
+        question: "question n°6 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 6"
+    },
+
+    // Question n°7  : 
+    { 
+        question: "question n°7 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 7"
+    },
+
+    // Question n°8  : 
+    { 
+        question: "question n°8 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 8"
+    },
+
+    // Question n°9  : 
+    { 
+        question: "question n°9 (à rédiger)",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 9"
+    },
+
+    // Question n°10  : 
+    { 
+        question: "question n°10 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 10"
+    },
+
+    // Question n°11  : 
+    { 
+        question: "question n°11 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 11"
+    },
+
+    // Question n°12  : 
+    { 
+        question: "question n°12 (à rédiger)",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 12"
+    },
+
+    // Question n°13  : 
+    { 
+        question: "question n°13 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 13"
+    },
+
+    // Question n°14  : 
+    { 
+        question: "question n°14 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 14"
+    },
+
+    // Question n°15  : 
+    { 
+        question: "question n°15 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 15"
+    },
+
+    // Question n°16  : 
+    { 
+        question: "question n°16 (à rédiger)",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 16"
+    },
+
+    // Question n°17  : 
+    { 
+        question: "question n°17 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 17"
+    },
+
+    // Question n°18  : 
+    { 
+        question: "question n°18 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 1,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 18"
+    },
+
+    // Question n°19  : 
+    { 
+        question: "question n°10 (à rédiger)",
+        answers: ["mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)", "bonne réponse (à rédiger)"], 
+        correct: 2,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 19"
+    },
+
+    // Question n°20  : 
+    { 
+        question: "question n°20 (à rédiger)",
+        answers: ["bonne réponse (à rédiger)", "mauvaise réponse (à rédiger)", "mauvaise réponse (à rédiger)"], 
+        correct: 0,
+        explanation: "explication de la bonne réponse (à rédiger) à la question 20"
+    }
+
+];
