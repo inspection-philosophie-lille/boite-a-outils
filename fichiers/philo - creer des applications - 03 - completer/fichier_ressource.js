da4973e8bb0bcbf96a55714c73354edb21b05579
inspection-philosophie-lille <daniel.boulagnon@ac-lille.fr>
2026-05-17 21:43:07 +0200

    Add files via upload

--- fichiers/philo - creer des applications - 03 - completer/fichier_ressource.js
+++ fichiers/philo - creer des applications - 03 - completer/fichier_ressource.js
@@ -1,27 +1,9 @@
-// REMARQUES
-// Pour mettre le texte en italique			Les bornes <i>...</i> ou <em>...</em>
-// Pour mettre le texte en gras				Les bornes <strong>...</strong> ou <b>...</b>
-// Pour mettre le texte en couleur adaptée	Les bornes <span style="color:red">...<span style="color:red">...</span>
-// Pour mettre un espace insécable			Utiliser &nbsp;
-
-// 1. TITRES DE L'APPLICATION
-
-const title = "application «&nbsp;compléter&nbsp;»";
-const subtitle = "compléter par glisser-déposer";
-
-// 2. TEXTES AVEC TROUS PRÉDÉFINIS
-
+// Généré le 17/05/2026 21:10:08
+const title = "texte à trous";
+const subtitle = "réviser en complétant des textes";
 const textesAvecTrous = [
-
-	{
-	texte: "Lorem **mots manquants 1** sit amet, **mots manquants 2** elit, sed do **mots manquants 3** incididunt ut labore et dolore **mots manquants 4**. Ut enim ad **mots manquants 5**, quis nostrud exertexte ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in **mots manquants 6** esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	source: "Prénom NOM, <i>Références du texte</i>, p.XXX",
-	},
-	{
-	texte: "At **mots manquants 1** et accusamus et iusto odio **mots manquants 2** qui blanditiis praesentium **mots manquants 3** atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident. Similique sunt in culpa qui officia deserunt mollitia animi, id est **mots manquants 4** fuga. Et **mots manquants 5** rerum facilis est et expedita distinctio. Nam **mots manquants 6**, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.",
-	source: "Prénom NOM, <i>Références du texte</i>, p.XXX",
-	},
-	{
-	texte: "Temporibus **mots manquants 1** et aut officiis **mots manquants 2** necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a **mots manquants 3**, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Ut enim ad **mots manquants 4**, quis nostrum exertexteem **mots manquants 5** suscipit laboriosam, nisi ut aliquid ex **mots manquants6** consequatur ?",
-	}
+	{ texte: "Lorem **mots manquants 1** sit amet, **mots manquants 2** elit, sed do **mots manquants 3** incididunt ut labore et dolore **mots manquants 4**. Ut enim ad **mots manquants 5**, quis nostrud exertexte ullamco **mots manquants 6** laboris nisi ut aliquip ex ea **mots manquants 7** commodo consequat. Duis aute irure dolor in reprehenderit in **mots manquants 8** esse cillum **mots manquants 9** dolore eu fugiat nulla **mots manquants 10** pariatur. Excepteur sint occaecat **mots manquants 11** cupidatat non proident, sunt in culpa qui officia **mots manquants 12** deserunt mollit anim id est laborum.", source: "Prénom1 NOM1, Références du texte, p.XXX" },
+	{ texte: "At **mots manquants 1** et accusamus et iusto odio **mots manquants 2** qui blanditiis praesentium **mots manquants 3** atque corrupti quos dolores et quas molestias **mots manquants 4** excepturi sint occaecati **mots manquants 5** cupiditate non provident. Similique sunt in **mots manquants 6** culpa qui officia deserunt mollitia animi, id est **mots manquants 7** fuga. Et **mots manquants 8** rerum facilis est et expedita distinctio. Nam **mots manquants 9**, cum soluta **mots manquants 10** nobis est eligendi optio **mots manquants 11** cumque nihil impedit quo minus **mots manquants 12** id quod maxime placeat facere possimus.", source: "Prénom2 NOM2, Références du texte, p.YYY" },
+	{ texte: "Temporibus **mots manquants 1** et aut officiis **mots manquants 2** necessitatibus saepe eveniet ut **mots manquants 3** et voluptates repudiandae sint et **mots manquants 4** molestiae non recusandae. Itaque **mots manquants 5** earum rerum hic tenetur a **mots manquants 6**, ut aut reiciendis voluptatibus maiores alias **mots manquants 7** consequatur aut perferendis **mots manquants 8** doloribus asperiores repellat. Ut enim ad **mots manquants 9**, quis nostrum exertexteem **mots manquants 10** suscipit laboriosam, nisi **mots manquants 11** ut aliquid ex **mots manquants 12** consequatur ?", source: "Prénom3 NOM3, Références du texte, p.ZZZ" }
 ];
+if (typeof module !== 'undefined') module.exports = { title, subtitle, textesAvecTrous };
