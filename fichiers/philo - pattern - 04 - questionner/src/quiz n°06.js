6d90492176fa6741aa2256a39bcee4377b019c80
inspection-philosophie-lille <daniel.boulagnon@ac-lille.fr>
2026-08-04 12:41:20 +0200

    Add files via upload

---
+++ fichiers/philo - pattern - 04 - questionner/src/quiz n°06.js
@@ -0,0 +1,95 @@
+window.allQuestions = [
+// l'ordre des réponses "true", "false", "false" n'a aucune importance 
+// puisque les réponses sont ensuite présentées aléatoirement
+
+	{
+	question: "question n°01 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°02 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°03 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°04 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°05 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°06 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°07 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°08 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°09 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	},
+	{
+	question: "question n°10 : énoncé de la question",
+	answers: [
+	{ text: "bonne réponse", correct: true },
+	{ text: "mauvaise réponse", correct: false },
+	{ text: "mauvaise réponse", correct: false }
+	],
+	explanation: "justification de la réponse correcte à la question"
+	}
+];
